36f63368d24dc701793ec75894b30bb83790c94d
Alyx25 <74509714+aylux257@users.noreply.github.com>
2024-03-01 15:22:55 +0100

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,56 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const cursor = document.querySelector('.cursor');
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]"); 
 
-  // Fonction pour mettre à jour la position du curseur
-  function updateCursorPosition(e) {
-    const posX = e.clientX;
-    const posY = e.clientY;
-    cursor.style.transform = `translate(${posX}px, ${posY}px)`;
+window.addEventListener("mousemove", function(e) {
+
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  //cursorOutline.style.left = `${posX}px`;
+  //cursorOutline.style.top = `${posY}px`;
+
+  cursorOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  }, {duration: 500, fill: "forwards"});
+
+  const hoveredElement = document.elementFromPoint(e.clientX, e.clientY);
+    
+  if (hoveredElement.tagName.toLowerCase() === 'a'|| hoveredElement.tagName.toLowerCase() === 'p' || hoveredElement.tagName.toLowerCase() === 'b') {
+    const hoveredElementColor = getComputedStyle(hoveredElement).color;
+    cursorOutline.style.borderColor = hoveredElementColor;
+  } else {
+    const body = document.querySelector('body');
+    if (body.style.backgroundColor == 'white'){
+      cursorOutline.style.borderColor = 'rgba(87, 87, 87, 0.5)'; 
+    }else{
+      cursorOutline.style.borderColor = 'rgba(210, 210, 210, 0.5)'; 
+    }
   }
-
-  // Mise à jour de la position initiale du curseur
-  updateCursorPosition({ clientX: window.innerWidth / 2, clientY: window.innerHeight / 2 });
-
-  // Gestion de l'événement de déplacement de la souris
-  document.addEventListener('mousemove', function(e) {
-    updateCursorPosition(e);
-    cursor.classList.add('active');
-  });
-
-  // Gestion de la fin de la transition
-  cursor.addEventListener('transitionend', function() {
-    cursor.classList.remove('active');
-  });
-
-  // Cursor Color for div='navbar-first'
-  const links = document.querySelectorAll('.navbar-first ul li a');
-
-  links.forEach(link => {
-    link.addEventListener('mouseover', () => {
-      cursor.classList.add('active');
-      cursor.style.borderColor = 'orange';
-      cursor.classList.add('hovered');
-    });
-    link.addEventListener('mouseleave', () => {
-      cursor.classList.remove('active');
-      cursor.style.borderColor = 'white';
-      cursor.classList.remove('hovered');
-    });
-  });
-
-  // Cursor Color for div='Accueil'
-  const AccueilLinks = document.querySelectorAll('#Accueil .all-navbar .navbar ul li a');
-
-  AccueilLinks.forEach(link => {
-    link.addEventListener('mouseover', () => {
-      cursor.classList.add('active');
-      cursor.style.borderColor = 'orange';
-      cursor.classList.add('hovered');
-    });
-    link.addEventListener('mouseleave', () => {
-      cursor.classList.remove('active');
-      cursor.style.borderColor = 'black';
-      cursor.classList.remove('hovered');
-    });
-  });
-});
+})
--- static/js/transition.js
+++ static/js/transition.js
@@ -2,73 +2,108 @@ document.addEventListener('DOMContentLoaded', function() {
     // First navbar
     const navbar = document.querySelector('.navbar-first');
 
+    const body = document.querySelector('body');
+
     // Button
     const accueilButton = document.getElementById('accueilButton');
     const monPortfolio = document.getElementById('monPortfolio');
     const aPropos = document.getElementById('aPropos');
     const contactButton = document.getElementById("contactButton");
-    const RetourFirstNavbar = document.getElementById("backToNavbar");
+    const RetourFirstNavbar = document.querySelectorAll("#backToNavbar");
+
 
     // Div
     const accueilDiv = document.getElementById("Accueil");
+    const portfolioDiv = document.getElementById("Mon-Portfolio");
+    const aProposDiv = document.getElementById("A-Propos");
+    const contacterDiv = document.getElementById("Contacter");
 
     // Cursor
-    const cursor = document.querySelector('.cursor');
+    const cursorDot = document.querySelector("[data-cursor-dot]");
+    const cursorOutline = document.querySelector("[data-cursor-outline]");
+
 
     accueilButton.addEventListener('click', function(e) {
         e.preventDefault();
+        body.style.backgroundColor = 'white';
         navbar.classList.add('slide-left');
         navbar.style.left = '-100%';
         navbar.classList.remove('slide-back');
         accueilDiv.style.display = 'flex';
         setTimeout(function() {
             navbar.style.display = 'flex';
+            cursorDot.style.backgroundColor = 'black';
+            cursorOutline.style.border = "2px solid rgba(87, 87, 87, 0.5)";
             accueilDiv.style.zIndex = '1';
-            cursor.style.borderColor = 'black';
         }, 500);
         
     });
 
     monPortfolio.addEventListener('click', function(e) {
         e.preventDefault();
+        body.style.backgroundColor = 'white';
         navbar.classList.add('slide-left');
+        navbar.style.left = '100%';
+        navbar.classList.remove('slide-back');
+        portfolioDiv.style.display = 'flex';
         setTimeout(function() {
-            cursor.style.borderColor = 'black';
-            navbar.style.display = 'none';
-            accueilDiv.style.display = 'block';
+            navbar.style.display = 'flex';
+            cursorDot.style.backgroundColor = 'black';
+            cursorOutline.style.border = "2px solid rgba(87, 87, 87, 0.5)";
+            portfolioDiv.style.zIndex = '1';
         }, 500);
     });
 
     aPropos.addEventListener('click', function(e) {
         e.preventDefault();
+        body.style.backgroundColor = 'white';
         navbar.classList.add('slide-left');
+        navbar.style.left = '100%';
+        navbar.classList.remove('slide-back');
+        aProposDiv.style.display = 'flex';
         setTimeout(function() {
-            cursor.style.borderColor = 'black';
-            navbar.style.display = 'none';
-            accueilDiv.style.display = 'block';
+            navbar.style.display = 'flex';
+            cursorDot.style.backgroundColor = 'black';
+            cursorOutline.style.border = "2px solid rgba(87, 87, 87, 0.5)";
+            aProposDiv.style.zIndex = '1';
         }, 500);
     });
 
     contactButton.addEventListener('click', function(e) {
         e.preventDefault();
+        body.style.backgroundColor = 'white';
         navbar.classList.add('slide-left');
+        navbar.style.left = '100%';
+        navbar.classList.remove('slide-back');
+        contacterDiv.style.display = 'flex';
         setTimeout(function() {
-            cursor.style.borderColor = 'black';
-            navbar.style.display = 'none';
-            accueilDiv.style.display = 'block';
+            navbar.style.display = 'flex';
+            cursorDot.style.backgroundColor = 'black';
+            cursorOutline.style.border = "2px solid rgba(87, 87, 87, 0.5)";
+            contacterDiv.style.zIndex = '1';
         }, 500);
     });
 
-    RetourFirstNavbar.addEventListener('click', function(e) {
-        e.preventDefault();
-        navbar.classList.add('slide-back');
-        navbar.style.left = '0%';
-        navbar.classList.remove('slide-left');
-        accueilDiv.style.zIndex = '-1';
-        setTimeout(function(){
-            cursor.style.borderColor = 'white';
-            navbar.style.display = 'flex';
-            accueilDiv.style.display = 'none';
-        })
-    })
+    RetourFirstNavbar.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            e.preventDefault();
+            navbar.classList.add('slide-back');
+            navbar.style.left = '0%';
+            navbar.classList.remove('slide-left');
+            body.style.backgroundColor = 'black';
+            accueilDiv.style.zIndex = '-1';
+            portfolioDiv.style.zIndex = '-1';
+            aProposDiv.style.zIndex = '-1';
+            contacterDiv.style.zIndex = '-1';
+            setTimeout(function(){
+                cursorDot.style.backgroundColor = 'white';
+                cursorOutline.style.border = "2px solid rgba(210, 210, 210, 0.5)";
+                navbar.style.display = 'flex';
+                accueilDiv.style.display = 'none';
+                portfolioDiv.style.display = 'none';
+                aProposDiv.style.display = 'none';
+                contacterDiv.style.display = 'none';
+            }, 500); 
+        });
+    }); 
 });
